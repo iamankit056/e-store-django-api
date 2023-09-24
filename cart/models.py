156e2e4e8b805babb3f456f213d516604ad0f7e5
Ankit <>
2023-09-24 16:07:31 +0530

    final commit

--- cart/models.py
+++ cart/models.py
@@ -5,7 +5,7 @@ from products.models import Product
 # Create your models here.
 class Cart(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    product = models.OneToOneField(Product, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
 
     def __str__(self):
         return f'{self.user} => {self.product}'
